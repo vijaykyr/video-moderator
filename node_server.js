87af920d020a49074e3c519504aee5ef6747811a
vijaykyr <vijaykyr@gmail.com>
2016-04-03 10:00:10 -0400

    Added video submission html form

--- node_server.js
+++ node_server.js
@@ -1,23 +1,46 @@
 //load required modules
 var http = require("http");
+var qs = require('querystring');
+var fs = require('fs');
 const child_process = require('child_process');
 
-//use http instance to create server, bind to port 8081
+
+//initialize
+var form = fs.readFileSync('html/form.html');
+
+//create server, bind to port 8081
 http.createServer(function (request, response) {
-
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-
-   // Response Body
-   response.end('Hello World\n');
+    if(request.method === "GET") {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end(form);
+    }
+    else if(request.method === "POST") {
+      var requestBody = '';
+      request.on('data', function(data) {
+        requestBody += data;
+        if(requestBody.length > 1e6) {
+          response.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/plain'});
+          response.end('413: Request Entity Too Large');
+        }
+      });
+      request.on('end', function() {
+        console.log(requestBody);
+        var formData = qs.parse(requestBody);
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write('<!doctype html><html><head><title>response</title></head><body>');
+        response.write('Thanks for the data!<br />User Name: '+formData.UserName);
+        response.write('<br />Repository Name: '+formData.Repository);
+        response.write('<br />Branch: '+formData.Branch);
+        response.end('</body></html>');
+      });
+    }
 }).listen(8081);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
 
 //Run python script and print results
-child_process.exec('python vid_moderator.py test_video_files/birds.mp4 AIzaSyCPZJ3_hlLTcdMtkBEzXHXIuGkmNn1TeFc',vidModeratorCB)
+//child_process.exec('python vid_moderator.py test_video_files/birds.mp4 AIzaSyCPZJ3_hlLTcdMtkBEzXHXIuGkmNn1TeFc',vidModeratorCB)
 
 //vid_moderator.py callback function
 function vidModeratorCB(error, stdout, stderr) {
