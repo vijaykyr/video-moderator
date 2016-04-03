f231d958ae50df0880ffd67f7520a62cc33ab1b0
vijaykyr <vijaykyr@gmail.com>
2016-04-02 20:48:09 -0400

    deleted django files

--- mysite/gke_teardown.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#delete external load balancer
-kubectl delete services video-moderator
-
-#Delete the running pods
-kubectl delete rc video-moderator
-
-#Delete the cluster
-gcloud container clusters delete video-moderator
