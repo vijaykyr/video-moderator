f63d7e3b7a0fe709a35976180ec0c20c90750fdc
vijaykyr <vijaykyr@gmail.com>
2016-04-09 08:55:56 -0400

    moved dockerfile to orchestration folder

--- orchestration scripts/local_deploy.sh
+++ orchestration scripts/local_deploy.sh
@@ -2,7 +2,7 @@
 #delete old container
 docker rm -f video_moderator
 #build image
-docker build -t gcr.io/vijays-sandbox/video-moderator:latest ..
+docker build -t gcr.io/vijays-sandbox/video-moderator:latest .
 #create new container from image, bind host port 80 to container port 8081
 docker run -dp 80:8081 --name=video_moderator gcr.io/vijays-sandbox/video-moderator:latest
 
