be5e0c658f377186297193e28b761f952515ab48
vijaykyr <vijaykyr@users.noreply.github.com>
2016-04-15 20:53:12 -0400

    Update local_deploy.sh

--- mysite/local_deploy.sh
+++ mysite/local_deploy.sh
@@ -3,7 +3,7 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker build -t gcr.io/vijays-sandbox/video-moderator:latest .
+docker build -t gcr.io/vijays-sandbox/video-moderator:django .
 
-docker run -itp 80:80 gcr.io/vijays-sandbox/video-moderator:latest
+docker run -itp 80:80 gcr.io/vijays-sandbox/video-moderator:django
 
